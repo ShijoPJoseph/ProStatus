24cea5d5dcb15648b218794a8335f90103841858
ShijoPJoseph <36472470+ShijoPJoseph@users.noreply.github.com>
2020-06-14 21:02:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -34,30 +34,34 @@ app.post ('/Question', function (req, resp) {
 	var jsobj = JSON.stringify(os.userInfo());
 	userObj = JSON.parse(jsobj);
 	// console.log(req.body.cmtType);
+	//var actUser = req.body.usrname + ' (' + userObj.username +'@' + os.hostname() + ')';
 	var actUser = req.body.usrname;
+	var inpMessage = req.body.depQuestion;
+	inpMessage = inpMessage.replace(':)', '&#128578;');
+	inpMessage = inpMessage.replace(':-)', '&#128578;');
 	
 	if ( req.body.cmtType == 'question')
 	{    var substring = "?";
 		
 		if (req.body.depQuestion.indexOf(substring) == -1)
 		{
-	strTowrite = '<p class="qa"> \n' + '<div class="q"><span>' + actUser +' : </span>' + req.body.depQuestion + ' ?</div><div class="a"><span>INTC Team : </span> Thanks for your question. We would soon come back</div></p>'
+	strTowrite = '<p class="qa"> \n' + '<div class="q"><span><b>' + actUser +'</b> : </span>' + inpMessage + ' ?</div><div class="a"><span><i>INTC Team</i> : </span> Thanks for your question. We would soon come back</div></p>'
 		}
 		else
 		{
-				strTowrite = '<p class="qa"> \n' + '<div class="q"><span>' + actUser +' : </span>' + req.body.depQuestion + '</div><div class="a"><span>INTC Team : </span> Thanks for your question. We would soon come back</div></p>'
+				strTowrite = '<p class="qa"> \n' + '<div class="q"><span><b>' + actUser +'</b> : </span>' + inpMessage + '</div><div class="a"><span><i>INTC Team</i> : </span> Thanks for your question. We would soon come back</div></p>'
 		}
 		
 	}
 	
 	if ( req.body.cmtType == 'comment')
 	{
-	strTowrite = '<p class="qa"> \n' + '<div class="q"><span>' + actUser +' : </span>' + req.body.depQuestion + ' </div><div class="a"><span>INTC Team : </span> Thanks for your comment</div></p>'
+	strTowrite = '<p class="qa"> \n' + '<div class="q"><span><b>' + actUser +'</b> : </span>' + inpMessage + ' </div><div class="a"><span><i>INTC Team</i> : </span> Thanks for your comment</div></p>'
 	}
 	
 	if ( req.body.cmtType == 'sugession')
 	{
-	strTowrite = '<p class="qa"> \n' + '<div class="q"><span>' + actUser +' : </span>' + req.body.depQuestion + ' </div><div class="a"><span>INTC Team : </span> Thanks for your Sugession. We value them</div></p>'
+	strTowrite = '<p class="qa"> \n' + '<div class="q"><span><b>' + actUser +'</b> : </span>' + inpMessage + ' </div><div class="a"><span><i>INTC Team</i> : </span> Thanks for your Sugession. We value them</div></p>'
 	}
 	
 	
